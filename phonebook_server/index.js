190d45b6729ef64f297f4f21fc90c4aa042613a4
spmanolo <manolosan2001@gmail.com>
2023-08-28 19:12:10 +0200

    part3

---
+++ phonebook_server/index.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>hola mundo</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/info', (request, response) => {
+  const ids = persons.map(person => { return person.id })
+  const maxId = Math.max(...ids)
+
+  const date = new Date().toDateString()
+
+  const res = `
+  <h2>Phonebook has info for ${maxId} people</h2>
+  <p>${date}</p>
+  `
+
+  response.send(res)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
